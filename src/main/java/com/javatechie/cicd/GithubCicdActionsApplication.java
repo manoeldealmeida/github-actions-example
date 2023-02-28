a3ccb31c03cf81f45cc496acecee5094db02f49a
manoel.dealmeida@gmail.com <manoel.dealmeida@gmail.com>
2023-02-28 09:36:09 -0300

    Add welcome api

--- src/main/java/com/javatechie/cicd/GithubCicdActionsApplication.java
+++ src/main/java/com/javatechie/cicd/GithubCicdActionsApplication.java
@@ -2,10 +2,19 @@ package com.javatechie.cicd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class GithubCicdActionsApplication {
 
+	
+	@GetMapping("/welcome")
+	public String welcome() {
+		return "Welcome javatechie!";
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(GithubCicdActionsApplication.class, args);
 	}
