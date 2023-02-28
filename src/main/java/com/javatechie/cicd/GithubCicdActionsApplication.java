7d9c28051bbc565f18b0322c56a39d79471a0d69
manoel.dealmeida@gmail.com <manoel.dealmeida@gmail.com>
2023-02-28 14:18:00 -0300

    Add check item api

--- src/main/java/com/javatechie/cicd/GithubCicdActionsApplication.java
+++ src/main/java/com/javatechie/cicd/GithubCicdActionsApplication.java
@@ -2,7 +2,9 @@ package com.javatechie.cicd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,6 +15,16 @@ public class GithubCicdActionsApplication {
 	@GetMapping("/welcome")
 	public String welcome() {
 		return "Welcome javatechie!";
+	}
+	
+	@GetMapping("/api/v1/is-valid-item")
+	public ResponseEntity isValidItem(@RequestParam("item") String item) {
+		
+		if(item == null || item.trim().isEmpty() || item.matches("\\d+"))
+			return ResponseEntity.badRequest().build();
+		else
+			return ResponseEntity.ok().build();
+		
 	}
 	
 	public static void main(String[] args) {
